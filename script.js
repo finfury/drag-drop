a10bd97c053db6c895eb6dd0c643165747712af3
mny <mail@mail.mail>
2024-04-03 19:02:51 +0300

    add control for touchpads

--- script.js
+++ script.js
@@ -1,55 +1,92 @@
+const isMobile = isMobileDevice()
 const control = document.getElementById('control')
 if (control) {
     initControl(control)
+}
+
+function isMobileDevice() {
+    return 'ontouchstart' in window || (window.DocumentTouch && document instanceof window.DocumentTouch) || navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0;
 }
 
 function initControl(control) {
 
     const pickItems = Array.from(control.getElementsByClassName('pick-item'))
     pickItems.forEach(item => {
+        const startToDragNameEvent = isMobile ? "touchstart" : "mousedown"
+        const moveNameEvent = isMobile ? "touchmove" : "mousemove"
+        const upEventName = isMobile ? "ontouchend" : "onmouseup"
         let targetList = null;
         let currentDroppable = null;
-        item.addEventListener('mousedown', holdDownHandler)
+
+        if (isMobile) {
+            item.addEventListener(startToDragNameEvent, holdDownHandler)
+        } else {
+            item.addEventListener(startToDragNameEvent, holdDownHandler)
+        }
 
         function holdDownHandler(event) {
             // Отмена браузерного обработчика перемещения
             item.ondragstart = () => false
 
             // Получаем координаты курсора при нажатии на элемент
-            let shiftX = event.clientX - item.getBoundingClientRect().left;
-            let shiftY = event.clientY - item.getBoundingClientRect().top;
+            let shiftX,
+                shiftY;
+            if (isMobile) {
+                shiftX = event.touches[0].clientX - item.getBoundingClientRect().left
+                shiftY = event.touches[0].clientY - item.getBoundingClientRect().top
+            } else {
+                shiftX = event.clientX - item.getBoundingClientRect().left;
+                shiftY = event.clientY - item.getBoundingClientRect().top;
+            }
 
             // Позиционируем элемент рядом с курсором
             item.style.position = 'absolute';
             item.style.zIndex = '5';
-            item.style.left = event.pageX - shiftX + 'px';
-            item.style.top = event.pageY - shiftY + 'px';
+            if (isMobile) {
+                console.log(event.touches[0])
+                item.style.left = event.touches[0].pageX - shiftX + 'px'
+                item.style.top = event.touches[0].pageY - shiftY + 'px'
+            } else {
+                console.log(event)
+                item.style.left = event.pageX - shiftX + 'px';
+                item.style.top = event.pageY - shiftY + 'px';
+            }
 
             const onCursorMove = (event) => {
                 moveItem(event, shiftX, shiftY)
             }
 
-            document.addEventListener('mousemove', onCursorMove);
-            document.onmouseup = () => {
+            document.addEventListener(moveNameEvent, onCursorMove);
+            document[upEventName] = () => {
                 item.style.position = 'static';
                 if (targetList) targetList.prepend(item)
 
-                document.removeEventListener('mousemove', onCursorMove);
-                document.onmouseup = null
+                document.removeEventListener(moveNameEvent, onCursorMove);
+                document[upEventName] = null
             }
         }
 
         const moveItem = (event, shiftX, shiftY) => {
             // Позиционируем элемент всегда рядом с курсором
-            item.style.left = event.pageX - shiftX + 'px';
-            item.style.top = event.pageY - shiftY + 'px';
+            if (isMobile) {
+                item.style.left = event.touches[0].pageX - shiftX + 'px'
+                item.style.top = event.touches[0].pageY - shiftY + 'px'
+            } else {
+                item.style.left = event.pageX - shiftX + 'px';
+                item.style.top = event.pageY - shiftY + 'px';
+            }
 
             // Прячем переносимый элемент, чтобы поймать событие elementFromPoint
             item.hidden = true;
             item.style.pointerEvents = "none";
 
             // Поиск самого глубокого элемента по дереву под перетаскиваемым элементом
-            let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+            let elemBelow;
+            if (isMobile) {
+                elemBelow = document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY);
+            } else {
+                elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+            }
 
             item.style.pointerEvents = "all";
             item.hidden = false;
